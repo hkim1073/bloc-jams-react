7eb053408bbbc3f0efd3f097def657664264b6e7
hkim1073 <hkim1073@gmail.com>
2019-07-11 19:47:05 -0400

    Added handleNextClick

--- src/components/Album.js
+++ src/components/Album.js
@@ -78,6 +78,14 @@ class Album extends Component {
     this.play();
   }
 
+  handleNextClick(){
+    const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
+    const newIndex = Math.max(0, currentIndex + 1);
+    const newSong = this.state.album.songs[newIndex];
+    this.setSong(newSong);
+    this.play();
+  }
+
   render() {
     return (
       <section className="album">
@@ -116,6 +124,8 @@ class Album extends Component {
            isPlaying={this.state.isPlaying}
            currentSong={this.state.currentSong}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
+           handlePrevClick={() => this.handlePrevClick()}
+           handleNextClick={() => this.handleNextClick()}
          />
       </section>
     );
